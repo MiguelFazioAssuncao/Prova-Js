dfb594646c7ca9ae376c705d6d045a56dd4d4dde
Miguel Fazio de Assunção <150205667+MiguelFazioAssuncao@users.noreply.github.com>
2024-08-21 08:44:54 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,32 @@
+let usuarios = []
+
+function adicionarUsuario(nome, idade, email) {
+    const usuario = {
+        nome,
+        idade,
+        email
+    }
+
+    if (idade > 0) {
+        usuarios.push(usuario)
+        console.log(`Usuário ${nome} adicionado com sucesso!`)
+    } else {
+        console.log("Idade inválida. Idade precisa ser um número positivo.")
+    }
+    
+    listarUsuarios(usuarios);
+}
+
+function listarUsuarios(usuarios) {
+    if (usuarios.length === 0) {
+        console.log("Nenhum usuário cadastrado.")
+        return
+    } else {
+        console.log(`Usuários cadastrados: ${usuarios.length}`)
+        usuarios.forEach((usuario, index) => {
+            console.log(`Nome: ${usuario.nome}, Idade: ${usuario.idade}, Email: ${usuario.email}`)
+        })
+    }
+}
+
+adicionarUsuario('Miguel', 17 , 'faziomiguel0@gmail.com')
